31ba4e124bffca0efb1baaef3d4071b3bdce3731
Henry T Kirk <henrytkirk@gmail.com>
2023-06-22 14:29:11 -0400

    Category for ensuring nav bar extends to notch.

---
+++ Classes/Categories/UIViewController+Navigation.h
@@ -0,0 +1,16 @@
+//
+//  UIViewController+Navigation.h
+//  DietMasterGoPlus
+//
+//  Created by Henry T Kirk on 6/21/23.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// Categories for navigation on a UIViewController.
+@interface UIViewController (DMNavigation)
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/Categories/UIViewController+Navigation.m
@@ -0,0 +1,19 @@
+//
+//  UIViewController+Navigation.m
+//  DietMasterGoPlus
+//
+//  Created by Henry T Kirk on 6/21/23.
+//
+
+#import "UIViewController+Navigation.h"
+
+@interface UIViewController (DMNavigation) <UIBarPositioningDelegate>
+@end
+
+@implementation UIViewController (DMNavigation)
+
+- (UIBarPosition)positionForBar:(id <UIBarPositioning>)bar {
+    return UIBarPositionTopAttached;
+}
+
+@end
